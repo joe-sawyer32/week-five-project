e56bcf70d7c863e33e19304d96bc6daa094c25aa
Joe <joe-sawyer32@users.noreply.github.com>
2017-06-23 14:00:36 -0500

    Mystery word displayed

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const session = require("express-session");
+const sessionConfig = require(path.join(__dirname, "sessionConfig"));
+
+// GAME SPECIFIC REQUIRES
+const mysteryWord = require(path.join(__dirname, "data.js"));
 
 // SET ENGINE
 app.engine("mustache", mustacheExpress());
@@ -15,10 +19,30 @@ app.set("views", path.join(__dirname, "/public"));
 
 // MIDDLEWARE
 app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
+function checkAuth(request, response, next) {
+  if (!request.session.user) {
+    response.redirect("/");
+  } else {
+    next();
+  }
+}
 
 // ROUTES
 app.get("/", (request, response) => {
   response.render("index");
+});
+
+app.post("/users", (request, response) => {
+  // need validation
+  request.session.user = request.body;
+  response.redirect("/game");
+});
+
+app.get("/game", checkAuth, (request, response) => {
+  response.send(mysteryWord);
+  //   response.render("game", { user: request.session.user });
 });
 
 app.listen(port, () => {
---
+++ sessionConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  secret: "spaceMan Orangutan elevatOrd 18",
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 600000 }
+};
